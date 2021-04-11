0e2d01a20995e5c7e731b326f6430be56a1f01ad
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-04-11 16:25:56 -0300

    Remoção de 'lixo'

--- trash/teste_autoloader.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once '../classes/core/AutoLoaderJB.php';
-
-$al = new Barcellos\Core\AutoLoaderJB();
-$al->register();
--- trash/teste_sessao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-
-//$_SESSION['1'] = 'teste';
-
-
-var_dump($_SESSION);
